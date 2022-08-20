12a5bc4eea7a969bf18d0fe5baa74c49af2f9d63
KonstantinKhan <khan366kos@gmail.com>
2022-08-20 19:28:08 +0500

    Implemented getting data of a random char.

--- src/components/randomChar/RandomChar.tsx
+++ src/components/randomChar/RandomChar.tsx
@@ -1,40 +1,80 @@
 import './randomChar.sass'
-import thor from '../../resources/img/thor.jpeg'
 import mjolnir from '../../resources/img/mjolnir.png'
+import {Component} from "react";
+import MarvelService from "../../services/MarvelService";
 
-const RandomChar = () => {
-    return (
-        <div className="random-char">
-            <div className="random-char__block">
-                <img src={thor} alt="thor"/>
-                <div className="random-char__block__info">
-                    <p className="random-char__block__info__name">thor</p>
-                    <p className="random-char__block__info__descr">As the Norse God of thunder and lightning, Thor
-                        wields one of the greatest weapons ever made, the
-                        enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile,
-                        he's quite smart and compassionate...</p>
-                    <div className="random-char__block__info__buttons-block">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">wiki</div>
-                        </a>
+interface State {
+    char: Char
+}
+
+interface Char {
+    name?: string,
+    description?: string,
+    thumbnail?: string,
+    homepage?: string,
+    wiki?: string
+}
+
+interface Props {
+
+}
+
+
+class RandomChar extends Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {char: {}}
+        this.updateChar()
+    }
+
+
+    marvelService = new MarvelService()
+
+    onCharLoaded = (char: Char) => {
+        this.setState({
+            char
+        })
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+        this.marvelService
+            .getCharacter(id).then(this.onCharLoaded)
+    }
+
+    render() {
+        const {char: {thumbnail, name, description, homepage, wiki}} = this.state
+        return (
+            <div className="random-char">
+                <div className="random-char__block">
+                    <img src={thumbnail} alt="thor"/>
+                    <div className="random-char__block__info">
+                        <p className="random-char__block__info__name">{name}</p>
+                        <p className="random-char__block__info__descr">{description}</p>
+                        <div className="random-char__block__info__buttons-block">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">wiki</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
+                <div className="random-char__static">
+                    <p className="random-char__title">Random character for today! <br/>
+                        Do you want to get to know him better?</p>
+                    <p className="random-char__title">Or choose another one</p>
+                    <a href="#" className="button button__main">
+                        <div className="inner">try it</div>
+                    </a>
+                    <img src={mjolnir} alt="mjolnir" className="random-char__decoration"/>
+                </div>
             </div>
-            <div className="random-char__static">
-                <p className="random-char__title">Random character for today! <br/>
-                    Do you want to get to know him better?</p>
-                <p className="random-char__title">Or choose another one</p>
-                <a href="#" className="button button__main">
-                    <div className="inner">try it</div>
-                </a>
-                <img src={mjolnir} alt="mjolnir" className="random-char__decoration"/>
-            </div>
-        </div>
 
-    )
+        )
+    }
 }
 
 export default RandomChar
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './services/MarvelService'
 import App from './components/app/App';
-import MarvelService from "./services/MarvelService";
-
-const marvelService = new MarvelService()
-
-// marvelService.getAllCharacters().then(res => res.data.results.forEach((item: any) => console.log(item.name)))
-// marvelService.getCharacter(1011052).then(res => res.data.results.forEach((item: any) => console.log(item.name)))
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
