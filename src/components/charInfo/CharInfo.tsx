727da5fe3502633d14f3f9b35704cc3c7ad8097c
KonstantinKhan <khan366kos@gmail.com>
2022-09-04 21:33:02 +0500

    Updated structure component.

--- src/components/charInfo/CharInfo.tsx
+++ src/components/charInfo/CharInfo.tsx
@@ -1,90 +1,147 @@
-import './charInfo.sass'
-import thor from '../../resources/img/thor.jpeg'
-import {Component} from "react";
-import {Char} from "../randomChar/RandomChar";
+import React, {Component} from "react";
+
+import {Char, Comic} from "../randomChar/RandomChar";
+import Spinner from "../spinner/Spinner"
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Skeleton from "../skeleton/Skeleton";
+
 import MarvelService from "../../services/MarvelService";
 
+import './charInfo.sass'
+
 interface Props {
-    charId: string
+    charId: number | null
 }
 
 interface State {
     char: Char,
     loading: boolean,
-    error: boolean
+    error: boolean,
 }
 
 class CharInfo extends Component<Props, State> {
 
-    state = {char: {}, loading: true, error: false}
+    state = {char: {}, loading: false, error: false}
 
     marvelService = new MarvelService()
+
+    componentDidMount() {
+        this.updateChar()
+    }
+
+    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
+        if (this.props.charId !== prevProps.charId) {
+            this.updateChar()
+        }
+    }
 
     updateChar = () => {
         const {charId} = this.props
         if (!charId) {
             return
         }
+
+        this.onCharLoading()
+
+        this.marvelService
+            .getCharacter(charId)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+
+    onCharLoaded = (char: Char) => {
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onCharLoading = () => {
+        this.setState(
+            {
+                loading: true
+            }
+        )
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
     }
 
     render() {
+
+        const {char, loading, error} = this.state
+
+        const skeleton = Object.keys(char).length !== 0 || loading || error ? null : <Skeleton/>
+
+        const errorMessage = error ? <ErrorMessage/> : null
+        const spinner = loading ? <Spinner/> : null
+        const content = !(loading || error || Object.keys(char).length === 0) ? <View {...char}/> : null
+
         return (
             <div className="char__info">
-                <div className="char__basics">
-                    <img src={thor} alt=""/>
-                    <div>
-                        <p className="char__info-name">Thor</p>
-                        <div className="char__btns">
-                            <a href="#" className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href="#" className="button button__secondary">
-                                <div className="inner">wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <p className="char__description">
-                    As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the
-                    enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's
-                    quite smart and compassionate...
-                </p>
-                <div>
-                    <p className="char__comics">Comics: </p>
-                    <li className="char__comics-item">
-                        All-Winners Squad: Band of Heroes (2011) #3
-                    </li>
-                    <li className="char__comics-item">
-                        Alpha Flight (1983) #50
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #503
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #504
-                    </li>
-                    <li className="char__comics-item">
-                        AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Vengeance (2011) #4
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1963) #1
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1996) #1
-                    </li>
-                </div>
+                {skeleton}
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
         )
     }
+}
+
+const View = (char: Char) => {
+
+    const {name, description, thumbnail, homepage, wiki, comics} = char
+
+    let imgStyle: React.CSSProperties = {objectFit: "cover"}
+    if (thumbnail?.includes("image_not_available")) {
+        imgStyle = {objectFit: "contain"}
+    }
+
+    const length = comics?.length ?? 0
+
+    return (
+        <>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} style={imgStyle}/>
+                <div>
+                    <p className="char__info-name">{name}</p>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">wiki</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <p className="char__description">
+                {description}
+            </p>
+            <div>
+                <p className="char__comics">Comics: </p>
+                <ul className="char__comics-list">
+                    {length > 0 ? null : 'There is comics with this character'}
+
+                    {
+                        comics?.map((item, index) => {
+                            if (index < 10) {
+                                return (
+                                    <li key={index} className="char__comics-item">
+                                        {item.name}
+                                    </li>
+                                )
+                            }
+                        })
+                    }
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default CharInfo
